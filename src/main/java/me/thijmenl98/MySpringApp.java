125697984d5874fb5e1c08b3f036d5899ed8fd99
TLangendam <thijmenlangendam@gmail.com>
2022-08-26 20:34:18 +0200

    Create spring-based raw prototype

---
+++ src/main/java/me/thijmenl98/MySpringApp.java
@@ -0,0 +1,30 @@
+package me.thijmenl98;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * <h3>Project: spring-bc</h3>
+ * <h5>File: MySpringApp</h5>
+ * <h5>Package: me.thijmenl98</h5>
+ *
+ * @author Thijmen Langendam
+ * @date 2022-08-26
+ **/
+public class MySpringApp {
+
+    public static void main(String[] args) {
+        // Load Spring Config File
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Retrieve Bean from Spring Container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        // Call Methods on Bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // Close Context
+        context.close();
+    }
+
+}
