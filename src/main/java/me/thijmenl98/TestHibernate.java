dee0fd6359b9048a0229201a9c2614e652dc64d0
TLangendam <thijmenlangendam@gmail.com>
2022-09-17 16:38:35 +0200

    Communicate with Hibernate

---
+++ src/main/java/me/thijmenl98/TestHibernate.java
@@ -0,0 +1,42 @@
+package me.thijmenl98;
+
+import me.thijmenl98.model.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * <h3>Project: spring-bc</h3>
+ * <h5>File: TestHibernate</h5>
+ * <h5>Package: me.thijmenl98</h5>
+ *
+ * @author Thijmen Langendam
+ * @date 2022-09-17
+ **/
+public class TestHibernate {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        try (factory) {
+            Session session = factory.getCurrentSession();
+            System.out.println("Creating new Student object");
+            Student student = new Student("Paul", "Walker", "paul_walker@gmail.com");
+
+            session.beginTransaction();
+
+            System.out.println("Saving student");
+            session.save(student);
+
+            session.getTransaction().commit();
+
+            System.out.println("Done");
+        }
+
+    }
+
+}
---
+++ src/main/java/me/thijmenl98/model/Student.java
@@ -0,0 +1,84 @@
+package me.thijmenl98.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * <h3>Project: spring-bc</h3>
+ * <h5>File: Student</h5>
+ * <h5>Package: me.thijmenl98.model</h5>
+ *
+ * @author Thijmen Langendam
+ * @date 2022-09-17
+ **/
+@Entity
+@Table(name="student")
+public class Student {
+
+    @Id
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    public Student() {
+
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
