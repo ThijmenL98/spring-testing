108e1dfbacc2de2fcd71c9aa5d315ef7cbec43ed
TLangendam <thijmenlangendam@gmail.com>
2022-09-20 16:33:06 +0200

    Setup new project for Hibernate Advanced Mappings

--- src/main/java/me/thijmenl98/model/Instructor.java
+++ src/main/java/me/thijmenl98/model/Instructor.java
@@ -4,35 +4,40 @@ import javax.persistence.*;
 
 /**
  * <h3>Project: spring-bc</h3>
- * <h5>File: Student</h5>
+ * <h5>File: Instructor</h5>
  * <h5>Package: me.thijmenl98.model</h5>
  *
  * @author Thijmen Langendam
- * @date 2022-09-17
+ * @date 2022-09-20
  **/
+
 @Entity
-@Table(name="student")
-public class Student {
+@Table(name = "instructor")
+public class Instructor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    @Column(name = "id")
     private int id;
 
-    @Column(name="first_name")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column(name="last_name")
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name="email")
+    @Column(name = "email")
     private String email;
 
-    public Student() {
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    private InstructorDetail instructorDetail;
+
+    public Instructor() {
 
     }
 
-    public Student(String firstName, String lastName, String email) {
+    public Instructor(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -70,13 +75,22 @@ public class Student {
         this.email = email;
     }
 
+    public InstructorDetail getInstructorDetail() {
+        return instructorDetail;
+    }
+
+    public void setInstructorDetail(InstructorDetail instructorDetail) {
+        this.instructorDetail = instructorDetail;
+    }
+
     @Override
     public String toString() {
-        return "Student{" +
+        return "Instructor {" +
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
+                ", instructorDetail=" + instructorDetail +
                 '}';
     }
 }
